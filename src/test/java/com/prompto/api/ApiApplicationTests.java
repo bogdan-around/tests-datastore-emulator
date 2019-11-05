aabd66d5b9e887ee3def9d256f42d4d1c94283d0
bogdan <bogdan.oloeriu@around.media>
2019-11-05 16:13:41 +0100

    fixed context tests

--- src/test/java/com/prompto/api/ApiApplicationTests.java
+++ src/test/java/com/prompto/api/ApiApplicationTests.java
@@ -1,9 +1,14 @@
 package com.prompto.api;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTestContextBootstrapper;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {SpringBootTestContextBootstrapper.class})
 class ApiApplicationTests {
 
 	@Test
