b47861829309ee3df8dce612543dcae162783ce2
bogdan <bogdan.oloeriu@around.media>
2019-11-08 10:58:01 +0100

    solved test controller

--- src/test/java/com/prompto/api/HelloControllerTest.java
+++ src/test/java/com/prompto/api/HelloControllerTest.java
@@ -3,7 +3,7 @@ package com.prompto.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -13,8 +13,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@SpringBootTest
 @AutoConfigureMockMvc
+@WebMvcTest
 class HelloControllerTest {
 
     @Autowired
